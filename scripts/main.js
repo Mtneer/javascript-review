f74642a980c85f05a5952562168c33ec1f7d5403
David Billups <davidtbillups@gmail.com>
2021-01-12 11:52:05 -0500

    Created Overly-Excited sub-dir

--- scripts/main.js
+++ scripts/main.js
@@ -123,3 +123,55 @@ console.log(scum)  // This should print "1. Galactic Scum" in the console
 const under = takeNumber("Underdogs")
 console.log(under)  // This should print "2. Underdogs" in the console
 
+// -------------------------------------------------------------------
+// Nightclub management wants to use this app at the front door during the event. Incorporate the logic you have written and add an HTML document and apply CSS to make a mobile version of Battle of the Bands.
+// --------------------------------------------------------------------
+
+
+// Practice: Cookout
+const hamburger = {
+	name: 'Hamburger',
+	type: 'beef',
+	cooked: false,
+}
+const zucchini = {
+	name: 'Zucchini',
+	type: 'vegetable',
+	cooked: false,
+}
+const chickenBreast = {
+	name: 'Chicken Breast',
+	type: 'chicken',
+	cooked: false,
+}
+const corn = {
+	name: 'Corn',
+	type: 'vegetable',
+	cooked: false,
+}
+const steak = {
+	name: 'Steak',
+	type: 'beef',
+	cooked: false,
+}
+
+// An array that is grouping the objects together.
+const foods = [hamburger, zucchini, chickenBreast, corn, steak];
+
+// An empty array that will store the objects after the `grill()` function cooks the food.
+const cookedFood = [];
+
+function grill (currentObject) {
+    // Modify the food so that it is cooked
+    currentObject.cooked = true;
+
+    // Put the cooked food into the appropriate array
+    cookedFood.push(currentObject);
+};
+
+// Your task is to iterate over the array of foods and invoke the function for each item so that the cookedFood array contains all of the items after they are cooked.
+for (let i=0; i<foods.length; i++) {
+    grill(foods[i])
+}
+console.log(cookedFood)
+
